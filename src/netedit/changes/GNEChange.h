92ae7b699868d787918068cbdfe958b6ef56e2c3
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2020-01-23 13:39:38 +0100

    Updated support of dataSets in GNENet. Refs #6461

--- src/netedit/changes/GNEChange.h
+++ src/netedit/changes/GNEChange.h
@@ -35,6 +35,7 @@
 // ===========================================================================
 class GNEAttributeCarrier;
 class GNEAdditional;
+class GNEDataSet;
 class GNEDemandElement;
 class GNEGenericData;
 class GNEEdge;
