bfe105d46547744fea4c2050d2d14ff3df5fe80a
Sefa Yilmaz <s.yilmaz@tu-braunschweig.de>
2020-03-28 12:35:54 +0100

    enable shell calls

--- tools/traci/main.py
+++ tools/traci/main.py
@@ -121,7 +121,7 @@ def start(cmd, port=None, numRetries=10, label="default", verbose=False):
         cmd2 = cmd + ["--remote-port", str(sumoPort)]
         if verbose:
             print("Calling " + ' '.join(cmd2))
-        sumoProcess = subprocess.Popen(cmd2)
+        sumoProcess = subprocess.Popen(cmd2, shell=True)
         try:
             return init(sumoPort, numRetries, "localhost", label, sumoProcess)
         except TraCIException:
